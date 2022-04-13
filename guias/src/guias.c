dc6ab0b9227f60b4c5515d379b616f2ddef8d438
Julieta Nakasone <julieta.enee@gmail.com>
2022-04-13 00:29:05 -0300

    pruebas y anotaciones con funcion continuarY

--- guias/src/guias.c
+++ guias/src/guias.c
@@ -9,6 +9,7 @@
  */
 
 #include <stdio.h>
+#include <stdio_ext.h>
 #include <stdlib.h>
 #include "utn.h"
 #include "tp.h"
@@ -21,12 +22,14 @@
 #define REINTENTOS 1
 
 int array_cargarAleatorio(int unArray[], int sizeArray, int* indice, int valor);
+int opcionContinuar(char* mensaje);
 
 int main(void) {
 
 	int arrayNum[ARRAY];
 	int ind;
 	int valorIngresado;
+	int respuesta;
 	ind = 0;
 
 	if(array_inicializar(arrayNum, ARRAY, INICIALIZAR)==0)
@@ -39,13 +42,34 @@ int main(void) {
 	{
 		array_imprimirArray(arrayNum, ARRAY);
 	}*/
-	if(!utn_GetNumeroInt(&valorIngresado, "ingrese valor: ", "ERROR", MINIMO, MAXIMO, REINTENTOS)
-			&& !array_cargarAleatorio(arrayNum, ARRAY, &ind, valorIngresado))
-	{
-		array_imprimirArray(arrayNum, ARRAY);
-	}
+
+	do{
+		/*do
+		{
+			utn_GetNumeroInt(&valorIngresado, "Ingrese valor: ", "ERROR", MINIMO, MAXIMO, REINTENTOS);
+		}while(valorIngresado == 0);PROBAR SI PODEMOS */
+
+		if(!utn_GetNumeroInt(&valorIngresado, "Ingrese valor: ", "ERROR", MINIMO, MAXIMO, REINTENTOS))
+		{
+			if(valorIngresado != 0 && !array_cargarAleatorio(arrayNum, ARRAY, &ind, valorIngresado))
+			{
+				array_imprimirArray(arrayNum, ARRAY);
+			}
+			else
+			{
+				printf("ERROR el valor debe ser distinto de 0\n");
+			}
+		}
+
+		respuesta= opcionContinuar("\npresione 'Y' para cargar otro. Presione cualquier tecla para cancelar");
 
 
+	}while(!respuesta);
+	printf("\nFINALIZANDO");
+
+
+
+	return EXIT_SUCCESS;
 }
 
 int array_cargarAleatorio(int unArray[], int sizeArray, int* indice, int valor)
@@ -64,6 +88,47 @@ int array_cargarAleatorio(int unArray[], int sizeArray, int* indice, int valor)
 }
 
 
+int opcionContinuar(char* mensaje)
+{
+	int retorno;
+	char respuesta;
+	retorno = -1;
+
+	printf("%s\nOpcion: ", mensaje);
+	__fpurge(stdin);
+	scanf("%c", &respuesta);
+	if(respuesta == 'Y')
+	{
+		respuesta = 'y';
+	}
+	if(respuesta == 'y')
+	{
+		retorno = 0;
+	}
+	return retorno;
+}
+
+/*
+		 * int opcionContinuar(char* mensaje, char respuesta)
+		{
+			int retorno;
+			char buffer;
+			int bufferInt;
+			int respuestaInt;
+			retorno =-1;
+
+			printf("%s", mensaje);
+			__fpurge(stdin);
+			scanf("%c", &buffer);
+			bufferInt= atoi(buffer);
+			respuestaInt= atoi(respuesta);
+			if(bufferInt+32 == respuestaInt)
+			{
+
+			}
+
+			return retorno;
+		}*/
 
 
 
